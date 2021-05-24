69e58e39fbb60eca45dbbadf2c4c1b96ea66c37a
meghraj <jagtapmeghraj05@gmail.com>
2021-05-24 10:31:51 +0530

    first commit

---
+++ src/com/carparking/Main.java
@@ -0,0 +1,9 @@
+package com.carparking;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is the main class");
+    }
+}
